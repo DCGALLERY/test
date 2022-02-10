0d88529a0b2d0cbb7333665a0e7be78b670adc2f
ledhcg <dinhcuong.firewin99@gmail.com>
2022-02-10 23:37:34 +0300

    test

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-function loadPortfolio(){
+var dataPortfolio, dataApp;
+
+function loadPortfolio(data){
   if( $('.list-photos-1').length ){
-    $.getJSON("portfolio.json", function(data){
       var listPhotos1 = `<div class="grid__item grid__item_desktop-4 grid__item_tablet-6 grid__item_mobile-12 grid__item_fluid-3 grid__sizer js-grid__sizer"></div>`;
       var listPhotos2 = `<div class="grid__item grid__item_desktop-6 grid__item_tablet-6 grid__item_mobile-12 grid__item_fluid-3 grid__sizer js-grid__sizer"></div>`;
       var listPhotos3 = `<div class="grid__item grid__item_desktop-3 grid__item_tablet-6 grid__item_mobile-12 grid__item_fluid-3 grid__sizer js-grid__sizer"></div>`;
@@ -101,7 +102,41 @@ function loadPortfolio(){
       $('.list-photos-2').html(listPhotos2);
       $('.list-photos-3').html(listPhotos3);
       $('.list-video').html(listVideo);
-    })
   }
 }
-loadPortfolio();
+function loadLink(data){
+      var domain = data["domain"];
+      var link_page = data["link-page"];
+      $(".js-href-home").prop("href", domain+link_page["home"]);
+      $(".js-href-blog").prop("href", domain+link_page["blog"]);
+      $(".js-href-portfolio").prop("href", domain+link_page["portfolio"]);
+      $(".js-href-about").prop("href", domain+link_page["about"]);
+      var link_social = data["link-social"];
+      $(".js-href-facebook").prop("href", domain+link_social["facebook"]);
+      $(".js-href-twitter").prop("href", domain+link_social["twitter"]);
+      $(".js-href-instagram").prop("href", domain+link_social["instagram"]);
+      $(".js-href-github").prop("href", domain+link_social["github"]);
+      $(".js-href-500px").prop("href", domain+link_social["500px"]);
+      
+}
+
+//Use in PJAXUpdateBody(components.js)
+function loadInfo(data){
+      var info = data["info"];
+      $(".js-info-email").prop("href", "mailto:"+info["email"]);
+      $(".js-info-email").html(info["email"]);
+      $(".js-info-phone").html(info["phone"]);
+      $(".js-info-address").html(info["address"]["stress"]+`<br/>`+info["address"]["city"]+`, `+info["address"]["country"]+`<br/>`+info["address"]["zip-code"]);
+      $(".js-current-year").html(new Date().getFullYear());
+}
+
+$.getJSON("portfolio.json", function(data){
+  dataPortfolio = data;
+  loadPortfolio(dataPortfolio);
+})
+$.getJSON("app.json", function(data){
+  dataApp = data;
+  loadLink(dataApp);
+  loadInfo(dataApp)
+})
+
